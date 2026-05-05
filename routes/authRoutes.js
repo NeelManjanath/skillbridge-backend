96f1b2eb8fe3783c5f8811622aff6585d0c836c5
Neel <neelmanjanath@gmail.com>
2026-05-05 22:05:45 +0530

    adding login backend

---
+++ routes/authRoutes.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const router = express.Router();
+const User = require("../models/User");
+
+// SIGNUP
+router.post("/signup", async (req, res) => {
+  try {
+    const { name, email, password, role } = req.body;
+
+    const user = new User({ name, email, password, role });
+    await user.save();
+
+    res.json({ message: "User created" });
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+// LOGIN
+router.post("/login", async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ email, password });
+
+    if (!user) {
+      return res.status(400).json({ message: "Invalid credentials" });
+    }
+
+    res.json({
+      message: "Login successful",
+      user
+    });
+
+  } catch (err) {
+    res.status(500).json({ error: err.message });
+  }
+});
+
+module.exports = router;
